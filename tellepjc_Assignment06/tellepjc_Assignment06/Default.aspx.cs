e78baad520d0304a7b92da55874061d0e1956f02
Connor <tellepjc@mail.uc.edu>
2017-02-22 20:22:29 -0500

    Set up DB Connection and framework for populating dropdowns

--- tellepjc_Assignment06/tellepjc_Assignment06/Default.aspx.cs
+++ tellepjc_Assignment06/tellepjc_Assignment06/Default.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,7 +10,137 @@ namespace tellepjc_Assignment06
 {
     public partial class Default : System.Web.UI.Page
     {
+        //declares all of the objects we will need to interact with the database
+        //they're static because we only want one connection to the database regardless of the amount of clients
+        private static System.Data.SqlClient.SqlConnection conn;
+        private static SqlCommand comm;
+        private static SqlDataReader reader;
+
         protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                // We will open the connection one time and leave it open.
+                OpenConnection();
+                // We will populate the dropdown menus once as their contents don't change often
+                PopulateDropdowns()
+            }
+        }
+
+        private void OpenConnection()
+        {
+            //creates a configuration setting object for the connection string
+            System.Configuration.ConnectionStringSettings strConn;
+            //sets the value of the connections setting object to the connection string
+            strConn = ReadConnectionString();
+
+            //initializes the connection object with the value of our connection string
+            conn = new System.Data.SqlClient.SqlConnection(strConn.ConnectionString);
+
+            // This could go wrong in so many ways...
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                // Miserable error handling...
+                Response.Write(ex.Message);
+            }
+        }
+
+        /**
+         * Returns a settings object that holds the connection string for the database
+         */
+        private System.Configuration.ConnectionStringSettings ReadConnectionString()
+        {
+            //string to store the path so the web.config file
+            String strPath;
+            strPath = HttpContext.Current.Request.ApplicationPath + "/web.config";
+
+            //creates an object that points to the web.config file
+            System.Configuration.Configuration rootWebConfig = System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration(strPath);
+
+            System.Configuration.ConnectionStringSettings connString = null;
+
+            //if the connection string is present, sets the object to equal the connection string in the web.config file
+            if (rootWebConfig.ConnectionStrings.ConnectionStrings.Count > 0)
+            {
+                connString = rootWebConfig.ConnectionStrings.ConnectionStrings["GroceryStoreSimulator"];
+            }
+
+            //returns our connection string settings object
+            return connString;
+        }
+
+        private void PopulateDropdowns()
+        {
+            PopulateLoyaltyList();
+            PopulateTransactionTypeList();
+            PopulateStoreList();
+            PopulateEmployeeList();
+            PopulateProductList();
+            PopulateCouponList();
+        }
+
+        private void PopulateLoyaltyList()
+        {
+            //variables to hold the data returned by the query and add it to the dropdown menu
+            int modelID;
+            string model;
+            ListItem modelItem;
+
+            // Clear the list box, in case we've already loaded something into it.
+            //drpModel.Items.Clear();
+            // create sql command object with the open connection object
+            comm = new SqlCommand("SELECT * FROM tRobo", conn);
+            //try to close the reader in case it's stil open, do nothing if we can't
+            try
+            {
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+            }
+
+            //use the reader object to execuet our query
+            reader = comm.ExecuteReader();
+
+            //iterate through the dataset line by line
+            while (reader.Read())
+            {
+                //stores the primary key of the model
+                modelID = reader.GetInt32(0);
+                //stores the name of the model
+                model = reader.GetString(1);
+                //creates a list item with the text of the name of the model, and the value of the primary key of the model
+                modelItem = new ListItem(model, modelID.ToString());
+                //adds the item to the dropdown menu
+                //drpModel.Items.Add(modelItem);
+            }
+        }
+
+        private void PopulateTransactionTypeList()
+        {
+
+        }
+
+        private void PopulateStoreList()
+        {
+
+        }
+
+        private void PopulateEmployeeList()
+        {
+
+        }
+
+        private void PopulateProductList()
+        {
+
+        }
+
+        private void PopulateCouponList()
         {
 
         }
